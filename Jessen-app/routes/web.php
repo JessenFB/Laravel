b29e76d39fbcb33f10e39eec5f217f1e95d73906
JessenFB <jessenfallikbemi_2327250065@mhs.mdp.ac.id>
2024-04-30 18:33:35 +0700

    p

--- Jessen-app/routes/web.php
+++ Jessen-app/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('About',function(){
+    return "Halaman About";
+});
+Route::get('profile',function(){
+    return view('profile');
+});
